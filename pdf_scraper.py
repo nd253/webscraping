7430dd6a1000a5788f0f71196ebcaea367ca6e2a
Nils Dambowy <nils.dambowy@googlemail.com>
2021-03-11 18:48:30 +0100

    Update pdf_scraper.py
    reworked the code and added comments

--- pdf_scraper.py
+++ pdf_scraper.py
@@ -1,26 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-#
-#  pdf_scraper.py
-#  
-#  Copyright 2021 Unknown <nils@t460>
-#  
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  (at your option) any later version.
-#  
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#  
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software
-#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-#  MA 02110-1301, USA.
-#  
-#  
 import requests
 import wget
 from bs4 import BeautifulSoup
@@ -33,11 +10,16 @@ def main():
 	soup = BeautifulSoup(page.content, 'html.parser')
 	
 	pdfs = []
-	
+	# search html code for links with
+	# '.pdf' ending
 	for link in soup.find_all('a'):
 		if link['href'].endswith('.pdf'):
 			pdfs.append(link.get('href'))
-	
+
+	# convert pfds to a complete url:
+	#ex. 'teaching/+altklausuren/ds_Klausur_SS_08.pdf' 
+	# => https://ae.cs.uni-frankfurt.de/teaching/+altklausuren/ds_Klausur_SS_08.pdf
+
 	urls = []
 	for i in range(len(pdfs)):
 		url = "https://ae.cs.uni-frankfurt.de/" + str(pdfs[i])
