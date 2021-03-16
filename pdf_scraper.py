b2002c8649ab0fa62ec1e5a833875428c76ff341
Nils Dambowy <nils.dambowy@googlemail.com>
2021-03-16 22:31:29 +0100

    Update pdf_scraper.py
    added user input, download status and a finishing message

--- pdf_scraper.py
+++ pdf_scraper.py
@@ -4,12 +4,13 @@ from bs4 import BeautifulSoup
 
 
 def main():
-	#URL = "https://ae.cs.uni-frankfurt.de/algo220"   
-	URL = "https://ae.cs.uni-frankfurt.de/teaching/altklausuren.html"
+	URL = str(input("Please enter the url: "))
+	
 	page = requests.get(URL)
 	soup = BeautifulSoup(page.content, 'html.parser')
 	
 	pdfs = []
+	
 	# search html code for links with
 	# '.pdf' ending
 	for link in soup.find_all('a'):
@@ -19,12 +20,21 @@ def main():
 	# convert pfds to a complete url:
 	#ex. 'teaching/+altklausuren/ds_Klausur_SS_08.pdf' 
 	# => https://ae.cs.uni-frankfurt.de/teaching/+altklausuren/ds_Klausur_SS_08.pdf
-
+	
 	urls = []
-	for i in range(len(pdfs)):
-		url = "https://ae.cs.uni-frankfurt.de/" + str(pdfs[i])
-		urls.append(url)
-		print(url)
+	
+	user_url = str(input("Does the pdf url match the entered url or is it different?(Y/N)"))
+	
+	if user_url  == "Y":
+		for i in range(len(pdfs)):
+			url = URL + str(pdfs[i])
+			urls.append(url)
+	
+	if user_url == "N":
+		new_url = str(input("Please enter the new url: "))
+		for i in range(len(pdfs)):
+			url = new_url + str(pdfs[i])
+			urls.append(url)
 	
 	
 	for u in urls:
@@ -32,12 +42,15 @@ def main():
 		file_name = u[file_name_start_pos:]
 		
 		try:
+			print("Downloading: ", u)
 			r = requests.get(u, auth=('algo220','algo220algo220'),stream=True)
 			if r.status_code == requests.codes.ok:
 				with open(file_name,'wb') as f:
 					for data in r:
 						f.write(data)
 		except:
-			print("error with: ", u)
+			print("Error with: ", u)
+	
+	print("Finished downloading", len(urls), " pdfs")
 if __name__ == '__main__':
     main()
